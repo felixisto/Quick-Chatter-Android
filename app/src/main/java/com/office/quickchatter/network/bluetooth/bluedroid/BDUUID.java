acf2a9952bdd9a72aab7c3f77503cda2512feddc
Kristiyan <felixisto@outlook.com>
2020-05-24 17:21:52 +0300

    Added comment

--- app/src/main/java/com/office/quickchatter/network/bluetooth/bluedroid/BDUUID.java
+++ app/src/main/java/com/office/quickchatter/network/bluetooth/bluedroid/BDUUID.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 
 public class BDUUID {
     public static @NonNull UUID get() {
+        // This called base UUID. Its a generic bluetooth uuid.
         return UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     }
 }
